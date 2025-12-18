80d38027dd938682631cbb96fd2f54f27094fe8f
azm197414 <azm197414@gmail.com>
2025-12-18 11:57:49 +0300

    Update online.js

--- online.js
+++ online.js
@@ -1,7 +1,7 @@
 // ========== ОНЛАЙН-СИНХРОНИЗАЦИЯ ДЛЯ ТУРНИРА "ВИХРЬ" ==========
 
 // КОНФИГУРАЦИЯ
-const GITHUB_TOKEN = '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'; // ЗАМЕНИТЕ НА СВОЙ!
+const GITHUB_TOKEN = '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'; // ЗАМЕНИТЕ НА СВОЙ!
 const REPO = 'azm197414/turnirvihr';
 const DATA_FILE = 'turnir_data.json';
 const DATA_URL = `https://raw.githubusercontent.com/${REPO}/main/${DATA_FILE}`;
